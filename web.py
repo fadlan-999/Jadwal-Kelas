28538519172897c1f35166cfb62c524944f5d4f5
fadlan-999 <fadlan.khairul.plb@gmail.com>
2026-08-13 21:21:16 +0700

    Update web.py

--- web.py
+++ web.py
@@ -44,7 +44,7 @@ password_siswa = {
 }
 
 # --- DAFTAR NAMA YANG DI-BLOKIR (Huruf kecil) ---
-daftar_blokir = ["fadlan"]
+daftar_blokir = []
 
 # --- SISTEM LOGIN SISWA ---
 st.subheader("🔒 Login Siswa")
