eac7953b4adea57d3067403315359be3c8a0073e
fadlan-999 <fadlan.khairul.plb@gmail.com>
2026-08-13 19:18:34 +0700

    Update web.py

--- web.py
+++ web.py
@@ -46,7 +46,7 @@ daftar_siswa = [
 
 # --- DAFTAR NAMA YANG DI-BLOKIR (Gunakan huruf kecil semua) ---
 # Contoh: ["budi", "joko"]
-daftar_blokir = [] 
+daftar_blokir = ["FADLAN"] 
 
 # --- GERBANG UTAMA (MENU PILIH NAMA / DROPDOWN) ---
 st.subheader("🔒 Verifikasi Pengunjung")
